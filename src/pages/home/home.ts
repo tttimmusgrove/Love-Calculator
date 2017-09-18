b042a75ed5015b3d3d7bfbf53b7147c6c7558e9f
Tim Musgrove <tim.musgrove91@gmail.com>
2017-09-17 22:33:53 -0600

    Finishing basic functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +6,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private name1: string = '';
+  private name2: string = '';
 
+  get score() {
+    const letters = (this.name1 + this.name2).toLowerCase();
+    let sum = 0;
+    for (let i = 0; i<letters.length; i++) {
+      sum += letters.charCodeAt(i);
+    }
+    return sum % 101;
   }
 
 }
