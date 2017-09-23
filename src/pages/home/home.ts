d16bd523fcef2c47086c3ddaebd25a767af825f6
Tim Musgrove <tim.musgrove91@gmail.com>
2017-09-23 13:34:56 -0600

    Finishing tutorial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contacts } from '@ionic-native/contacts';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,12 @@ export class HomePage {
   private name1: string = '';
   private name2: string = '';
 
+  constructor(
+    private contacts: Contacts
+  ) {
+
+  }
+
   get score() {
     const letters = (this.name1 + this.name2).toLowerCase();
     let sum = 0;
@@ -16,6 +23,26 @@ export class HomePage {
       sum += letters.charCodeAt(i);
     }
     return sum % 101;
+  }
+
+  pickContact1() {
+    this.contacts.pickContact()
+    .then((contact) => {
+      this.name1 = contact.displayName;
+    })
+    .catch(error => {
+      // ignore; user didn't select a contact
+    })
+  }
+
+  pickContact2() {
+    this.contacts.pickContact()
+    .then((contact) => {
+      this.name2 = contact.displayName;
+    })
+    .catch(error => {
+      // ignore; user didn't select a contact
+    })
   }
 
 }
